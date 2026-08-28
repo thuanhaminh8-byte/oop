e45b0ca94aa20a1611bda1c1a30561a6d748e080
thuanhaminh8-byte <thuanhaminh8@gmail.com>
2026-08-28 17:54:25 +0700

    Delete Restaurant/scr directory

--- Restaurant/scr/Intersection.java
+++
@@ -1,9 +0,0 @@
-public class Intersection {
-    private String avenue;
-    private String street;
-
-    public Intersection (String avenue, String street) {
-        this.avenue = avenue;
-        this.street = street;
-    }
-}
