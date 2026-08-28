91c470b7d097ff944b3cf3f840c1022bd428108f
thuanhaminh8-byte <thuanhaminh8@gmail.com>
2026-08-28 17:54:36 +0700

    Delete Train/scr directory

--- Train/scr/Clocktime.java
+++
@@ -1,9 +0,0 @@
-public class Clocktime {
-    private int hour;
-    private int minute;
-    public Clocktime(int hour, int minute)
-    {
-        this.hour = hour;
-        this.minute = minute;
-    }
-}
